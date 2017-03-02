51f01030bb63589364770b6dc63596b78cb89f67
Roahl <sapeben@gmail.com>
2017-03-02 10:14:05 +0100

    Addedd full functionality

---
+++ AgendaClientesWPF/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class Cliente
+    {
+        public string Cif { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Contact { get; set; }
+        public string Email { get; set; }
+
+        public Cliente(string cif, string name, string phone, string contact, string email)
+        {
+            Cif = cif;
+            Name = name;
+            Phone = phone;
+            Contact = contact;
+            Email = email;
+        }
+    }
+}
--- AgendaClientesWPF/MainWindow.xaml.cs
+++ AgendaClientesWPF/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
+﻿using Modelo;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AgendaClientesWPF
 {
@@ -23,6 +14,45 @@ namespace AgendaClientesWPF
         public MainWindow()
         {
             InitializeComponent();
+
+            List<Cliente> clientes = new List<Cliente>();
+
+
+            lboxEmpresas.DisplayMemberPath = "Name";
+            lboxEmpresas.Items.Add(new Cliente("A87745124", "Allstar Asociados", "986747125", "Manuel Castro", "metales@pedrin.info"));
+            lboxEmpresas.Items.Add(new Cliente("B23467889", "Relampago Juntitos", "986744251", "Sarah Johnson", "sarah@A.info"));
+            lboxEmpresas.Items.Add(new Cliente("A12346864", "Barrio de Aquí", "986777415", "Rachele Tyler", "rachele@A.info"));
+            lboxEmpresas.Items.Add(new Cliente("A86742466", "Elegante Total", "986688522", "Charlotte Sellers", "charlotte@A.info"));
+            lboxEmpresas.Items.Add(new Cliente("A56353426", "OneClick OneKill", "986117695", "Rebecca Hunter", "rebecca@A.info"));
+            lboxEmpresas.Items.Add(new Cliente("F68642362", "Celestial Cloud", "986122778", "Scarlett McMahon", "scarlett@A.info"));
+            lboxEmpresas.Items.Add(new Cliente("E90642610", "Prosperidad Carilla", "986110782", "Jane Smith", "jane@A.info"));
+        }
+
+
+        private void objetoSeleccionado(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                Cliente c = (Cliente)lboxEmpresas.SelectedItem;
+                txCif.Content = c.Cif;
+                txNombre.Content = c.Name;
+                txTelefono.Content = c.Phone;
+                txContacto.Content = c.Contact;
+                txEmail.Content = c.Email;
+            }catch(NullReferenceException)
+            {
+
+            }
+        }
+
+        private void btBorrar_Click(object sender, RoutedEventArgs e)
+        {
+            lboxEmpresas.Items.Remove(lboxEmpresas.SelectedItem);
+        }
+
+        private void btCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
